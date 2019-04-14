2993a9c054b99168a3c1dd6054e2b47f5912aef2
Noah Flint <noah.flint24@gmail.com>
2019-04-14 15:18:46 -0600

    refactor

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Dashboard::DashboardController < Dashboard::BaseController
   def index
     @merchant = current_user
     @pending_orders = Order.pending_orders_for_merchant(current_user.id)
-    @default_images = @merchant.items.default_images
+    @default_images = @merchant.default_images
   end
 end
