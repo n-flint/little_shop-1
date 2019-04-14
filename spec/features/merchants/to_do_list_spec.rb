2948211569f5ffb1e22e76b25a1ed06b403be075
Noah Flint <noah.flint24@gmail.com>
2019-04-14 16:30:13 -0600

    added test for item names to be links

--- spec/features/merchants/to_do_list_spec.rb
+++ spec/features/merchants/to_do_list_spec.rb
@@ -8,19 +8,35 @@ RSpec.describe 'Merchant Dashboard To-Do List' do
     it 'can see list of items that have default images' do
       merchant = create(:merchant)
       item_1 = create(:item, name: "Item 1", price: 10.00, inventory: 10, user: merchant, image: "https://target.scene7.com/is/image/Target/GUEST_848c9bef-75a4-4372-896d-4207a2278983?wid=488&hei=488&fmt=pjpeg")
-      item_2 = create(:item, name: "Item 2", price: 20.00, inventory: 20, user: merchant, image: '')
-      item_3 = create(:item, name: "Item 3", price: 30.00, inventory: 30, user: merchant, image: '')
+      item_2 = create(:item, name: "Item 2", price: 20.00, inventory: 20, user: merchant)
+      item_3 = create(:item, name: "Item 3", price: 30.00, inventory: 30, user: merchant)
       
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
 
       visit dashboard_path
-      # save_and_open_page
 
       within '.default-images' do
         expect(page).to_not have_content(item_1.name)
         expect(page).to have_content(item_2.name)
         expect(page).to have_content(item_3.name)
       end
+    end
+
+    it 'list of items that have default images are links to edit page' do
+      merchant = create(:merchant)
+      item_1 = create(:item, name: "Item 1", price: 10.00, inventory: 10, user: merchant, image: "https://target.scene7.com/is/image/Target/GUEST_848c9bef-75a4-4372-896d-4207a2278983?wid=488&hei=488&fmt=pjpeg")
+      item_2 = create(:item, name: "Item 2", price: 20.00, inventory: 20, user: merchant)
+      item_3 = create(:item, name: "Item 3", price: 30.00, inventory: 30, user: merchant)
+      
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+
+      visit dashboard_path
+
+      within '.default-images' do
+        click_link item_2.name
+      end
+      
+      expect(current_path).to eq(edit_dashboard_item_path(item_2))
     end
   end
 end
