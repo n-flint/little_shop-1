aa1684c6ecbab88334f59576957c51a6bcddb01f
Noah Flint <noah.flint24@gmail.com>
2019-04-15 15:25:36 -0600

    created test for warning merchant if item exceeds quatity

--- spec/features/merchants/to_do_list_spec.rb
+++ spec/features/merchants/to_do_list_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe 'Merchant Dashboard To-Do List' do
       @item_1 = create(:item, name: "Item 1", price: 50, inventory: 10, user: @merchant_1)
       @item_2 = create(:item, name: "Item 2", price: 20, inventory: 10, user: @merchant_1)
       @item_3 = create(:item, name: "Item 3", price: 70, inventory: 10, user: @merchant_1)
-      @item_4 = create(:item, name: "Item 3", price: 70, inventory: 10, user: @merchant_2)
+      @item_4 = create(:item, name: "Item 4", price: 70, inventory: 10, user: @merchant_2)
 
       @user = create(:user)
       @order_1 = create(:order, user: @user)
@@ -70,6 +70,32 @@ RSpec.describe 'Merchant Dashboard To-Do List' do
     it 'can see a statistic about unfulfilled items' do
       within '.unfulfilled-items' do
         expect(page).to have_content('You have 3 unfulfilled orders worth $600.00')
+      end
+    end
+    # describe 'inventory warnings' do
+    it 'can see warning if item ordered exceeds the quantity' do
+      merchant_1 = create(:merchant)
+      item_1 = create(:item, name: "Item 1", price: 50, inventory: 10, user: merchant_1)
+      item_2 = create(:item, name: "Item 2", price: 20, inventory: 10, user: merchant_1)
+      item_3 = create(:item, name: "Item 3", price: 70, inventory: 10, user: merchant_1)
+      # item_4 = create(:item, name: "Item 4", price: 70, inventory: 10, user: merchant_2)
+
+      user = create(:user)
+      order_1 = create(:order, user: user)
+      order_2 = create(:order, user: user)
+      order_3 = create(:order, user: user)
+      order_4 = create(:order, user: user)
+      create(:order_item, order: order_1, item: item_1, quantity: 10, price: 500)
+      create(:order_item, order: order_1, item: item_1, quantity: 10, price: 500)
+      create(:order_item, order: order_3, item: item_3, quantity: 5, price: 350)
+      create(:order_item, order: order_4, item: item_3, quantity: 4, price: 280)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1)
+
+      visit dashboard_path
+
+      within "#order-#{order_1.id}" do
+        expect(page).to have_content('Quantity of this item exceeds your inventory.')
       end
     end
   end
