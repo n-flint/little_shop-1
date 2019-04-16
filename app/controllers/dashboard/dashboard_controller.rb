dd0cca78453d4e4f482e6364bbcb1d6efea8d821
Noah Flint <noah.flint24@gmail.com>
2019-04-16 16:54:37 -0600

    created existing and potential actions

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -2,5 +2,22 @@ class Dashboard::DashboardController < Dashboard::BaseController
   def index
     @merchant = current_user
     @pending_orders = Order.pending_orders_for_merchant(current_user.id)
+    
+  end
+
+  def existing
+    @users = User.all
+    
+    respond_to do |format|
+      format.csv { send_data @users.to_csv}
+    end
+  end
+  
+  def potential
+    @users = User.all
+    
+    respond_to do |format|
+      format.csv { send_data @users.to_csv}
+    end
   end
 end
